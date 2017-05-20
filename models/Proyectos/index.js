bdb9e0adcf8af874d067ee12a41280af9a0ca16e
sky_k <sky_karlos@hotmmail.com>
2017-05-19 21:26:21 -0500

    arreglar para mostrar todos los proyectos (incluso en los que soy miembros y no solo los que soy dueño).

--- models/Proyectos/index.js
+++ models/Proyectos/index.js
@@ -24,27 +24,38 @@ exports.getAll = function(creador,done) {
 };
 
 
-exports.create = function(nombre, creador , rol, done) {
-    let values= [nombre, creador, rol];
+exports.create = function(nombre, creador, rol, done) {
+    let values= [nombre, creador];
+    let roli=rol;
+    let insertIDI;
 
     db.get().getConnection(function (err,connection) {
         if (err){
-            connection.release();
             console.log(err);
             return done(err);
         } else {
-            connection.query('INSERT INTO proyecto (nombre, creador, rol) VALUES(?, ?, ?)', values, function(err, result) {
+            connection.query('INSERT INTO proyecto (nombre, creador) VALUES(?, ?)', values, function(err, result) {
                 if (err) {
                     console.log(err);
-                    return done(err);
-                }
+                    return done(err,null);
+                }else {
+                  insertIDI=result.insertId;
+                  const valuestwo=[creador,insertIDI,roli,1];
+                    connection.query('INSERT INTO miembros (id_user, proyecto_id, rol, dueño) VALUES(?, ?, ?, ?)', valuestwo, function(errtwo, resultwo) {
+                        if (err) {
+                            console.log(errtwo);
+                            return done(errtwo);
+                        }else {
 
-                done(null, result.insertId);
+                            done(null, resultwo.insertId);
+                        }
+                    });
+                }
             });
 
-            connection.release();
-        }
 
+        }
+        connection.release();
     });
 };
 
